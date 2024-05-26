cd8a16e3e9303f311fcc2fa291cb016e5bd11659
Hessam <hessam2004h@gmail.com>
2024-05-27 03:27:27 +0330

    :passport_control: JWT Completed

--- src/main/java/com/nessam/server/utils/JWTManager.java
+++ src/main/java/com/nessam/server/utils/JWTManager.java
@@ -1,15 +1,15 @@
 package com.nessam.server.utils;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 import java.security.Key;
 import java.util.Date;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
 public class JWTManager {
     private final Key secretKey = Keys.hmacShaKeyFor("WithGreatPowerComesGreatResponsibilityRememberThat".getBytes());
@@ -20,21 +20,12 @@ public class JWTManager {
         Date now = new Date(nowMillis);
         Date exp = new Date(nowMillis + (long) expirationMinutes * 60 * 1000);
 
-        return Jwts.builder()
-                .setClaims(payload)
-                .setIssuedAt(now)
-                .setExpiration(exp)
-                .signWith(secretKey, algorithm)
-                .compact();
+        return Jwts.builder().setClaims(payload).setIssuedAt(now).setExpiration(exp).signWith(secretKey, algorithm).compact();
     }
 
     public Map<String, Object> decodeToken(String token) {
         try {
-            Claims claims = Jwts.parserBuilder()
-                    .setSigningKey(secretKey)
-                    .build()
-                    .parseClaimsJws(token)
-                    .getBody();
+            Claims claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody();
             return new HashMap<>(claims);
         } catch (JwtException e) {
             System.out.println("Invalid or expired token: " + e.getMessage());
