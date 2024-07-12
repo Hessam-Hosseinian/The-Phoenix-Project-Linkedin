abce3ac7abdf53caff5572034e3864980e826989
Hessam <hessam2004h@gmail.com>
2024-07-12 13:16:18 +0330

    Server class refactored :white_check_mark:

--- src/main/java/com/nessam/server/Server.java
+++ src/main/java/com/nessam/server/Server.java
@@ -2,10 +2,14 @@ package com.nessam.server;
 
 import com.nessam.server.config.Configuration;
 import com.nessam.server.config.ConfigurationManager;
+import com.nessam.server.handlers.httpHandlers.FileDownloadHandler;
+import com.nessam.server.handlers.httpHandlers.FileUploadHandler;
 import com.nessam.server.handlers.httpHandlers.RequestHandler;
 import com.nessam.server.handlers.modelHandlers.*;
 import com.nessam.server.utils.BetterLogger;
 import com.sun.net.httpserver.HttpServer;
+
+
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -15,7 +19,6 @@ import java.sql.SQLException;
 
 
 public class Server {
-
 
     public static void main(String[] args) {
 
@@ -27,27 +30,32 @@ public class Server {
         BetterLogger.INFO("Using WebRoot: " + conf.getWebroot());
 
 
+
         try {
 
             HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", conf.getPort()), 0);
 
             Files.createDirectories(Paths.get("src/main/java/com/nessam/server/assets"));
             server.createContext("/users", new UserHandler());
-
-//            server.createContext("/education", new EducationHandler());
             server.createContext("/req", new RequestHandler());
+            server.createContext("/education", new UserEducationHandler());
+            server.createContext("/jobPosition", new JobPositionHandler());
             server.createContext("/follows", new FollowHandler());
             server.createContext("/message", new MessageHandler());
             server.createContext("/post", new PostHandler());
             server.createContext("/likes", new LikeHandler());
             server.createContext("/comment", new CommentHandler());
             server.createContext("/search", new SearchHandler());
-            server.createContext("/hashtag", new HashtagHandler());
+            server.createContext("/upload", new FileUploadHandler());
+            server.createContext("/download", new FileDownloadHandler());
             server.setExecutor(null);
             server.start();
 
-        } catch (IOException | SQLException e) {
+
+        } catch (IOException e) {
             BetterLogger.ERROR(e.getMessage());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
 
     }
